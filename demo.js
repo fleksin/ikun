abccfa2238aea68254408e26b0f87b6bc91d2e88
fleksin <ppuu@163.com>
2022-10-31 11:48:11 +0800

    chore: updated demo

--- demo.js
+++ demo.js
@@ -11,11 +11,35 @@ function sleep(m) {
 }
 
 (async () => {
-  const inst = ikunDance("loading first one");
-  await sleep(4000);
-  inst.succeed("!!succeed !!!!");
+  console.time('timer')
+  console.log('Running Ikun....');
+  await sleep(1660);
+  console.log('全民制作人们 大家好。。。。')
+  await sleep(1660);
+  console.log('我是练习时长两年半的。。。。')
+  await sleep(1660);
+  console.log('个人练习生 蔡徐坤。。。。')
+  await sleep(1660);
+  console.log('喜欢唱 跳 rap 篮球。。。。')
+  await sleep(1660);
+  console.log('music !')
+  await sleep(5000);
+  const inst = ikunBasketball("loading");
+  await sleep(10000);
+  inst.failed("!!failed !!!!");
   await sleep(2000);
-  const inst2 = ikunBasketball("loading second one");
+  const inst2 = ikunDance("loading");
   await sleep(4000);
-  inst2.failed("!!failed !!!!");
+  inst2.succeed("!!succeed !!!!");
+  // console.timeEnd('timer')
 })();
+
+// (async () => {
+//   const inst = ikunDance("loading first one");
+//   await sleep(4000);
+//   inst.succeed("!!succeed !!!!");
+//   await sleep(2000);
+//   const inst2 = ikunBasketball("loading second one");
+//   await sleep(4000); 
+//   inst2.failed("!!failed !!!!");
+// })();
